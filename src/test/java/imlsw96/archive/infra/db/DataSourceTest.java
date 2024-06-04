b27d73f31682535b5aca10393c86f132c2983753
stillgood96 <a787574@naver.com>
2024-06-04 17:51:25 +0900

    feat: JPA & Lombok 의존성 추가 및 테스트코드 작성

---
+++ src/test/java/imlsw96/archive/infra/db/DataSourceTest.java
@@ -0,0 +1,35 @@
+package imlsw96.archive.infra.db;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.persistence.EntityManagerFactory;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@SpringBootTest
+public class DataSourceTest {
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Autowired
+  private EntityManagerFactory entityManagerFactory;
+
+  @Autowired
+  private PlatformTransactionManager transactionManager;
+
+  @Test
+  void testDataSource() throws SQLException {
+    assertThat(dataSource).isNotNull();
+    assertThat(entityManagerFactory).isNotNull();
+    assertThat(transactionManager).isNotNull();
+    assertThat(transactionManager).isInstanceOf(JpaTransactionManager.class);
+  }
+}
