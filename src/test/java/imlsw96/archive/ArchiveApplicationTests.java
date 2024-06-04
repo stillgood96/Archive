b1527fe00bb46cea1a25d6665077d9bd1a0fbe55
stillgood96 <a787574@naver.com>
2024-06-04 16:53:10 +0900

    Initial commit

---
+++ src/test/java/imlsw96/archive/ArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package imlsw96.archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiveApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
